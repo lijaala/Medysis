f28b72d80a436e45ba0e1e7d8cd508dcba363416
lijaala <np01cp4a220195@islingtoncollege.edu.np>
2024-12-21 21:10:01 +0545

    Error with autheticating users from staff table

--- Project/src/main/java/Medysis/Project/Controller/AuthController.java
+++ Project/src/main/java/Medysis/Project/Controller/AuthController.java
@@ -90,7 +90,7 @@ public class AuthController {
             response.sendRedirect("/login");
         }
         else{
-            response.sendRedirect("/login");
+            response.sendRedirect("/appointment");
 
         }
     }
